b84c3b383e006ccc733f18b37475f761a87f13b0
Diakon5 <diakonmanager@gmail.com>
2023-12-17 15:31:11 +0100

    Made "progress"

---
+++ MusicPlayer/MusicPlayer/Converters/DividerConverter.cs
@@ -0,0 +1,32 @@
+﻿using Avalonia.Data;
+using Avalonia.Data.Converters;
+using System;
+using System.Globalization;
+
+
+namespace MusicPlayer.Converters
+{
+    public class DividerConverter :IValueConverter
+    {
+        public static readonly DividerConverter Instance = new();
+
+        public object? Convert(object? value, Type targetType, object? parameter,
+                                                                CultureInfo culture)
+        {
+            if (value is float sourceValue && parameter is float divisionAmount
+                && divisionAmount != 0 &&targetType.IsAssignableTo(typeof(float)))
+            {
+                return sourceValue / divisionAmount;
+            }
+            // converter used for the wrong type
+            return new BindingNotification(new InvalidCastException(),
+                                                    BindingErrorType.Error);
+        }
+
+        public object ConvertBack(object? value, Type targetType,
+                                    object? parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
