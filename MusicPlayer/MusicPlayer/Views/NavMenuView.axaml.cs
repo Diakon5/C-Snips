61ecb05c98685bbe5024fd3befa9eb59046446bb
Diakon5 <diakonmanager@gmail.com>
2023-12-20 07:47:10 +0100

    Regress

--- MusicPlayer/MusicPlayer/Views/NavMenuView.axaml.cs
+++ MusicPlayer/MusicPlayer/Views/NavMenuView.axaml.cs
@@ -1,11 +1,14 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using System;
 
 namespace MusicPlayer.Views;
 
 public partial class NavMenuView : UserControl
 {
+    public double MaxHeightImageCalculated { get; set; } = 0;
     public NavMenuView()
     {
         InitializeComponent();
