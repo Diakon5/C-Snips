7386ceee38c9112468c1f639345d6e3614e7ac54
Diakon5 <72417806+Diakon5@users.noreply.github.com>
2024-01-09 14:31:30 +0100

    Must implement my own recursive search

--- MusicPlayer/MusicPlayer.Android/FetchMusicService.Android.cs
+++ MusicPlayer/MusicPlayer.Android/FetchMusicService.Android.cs
@@ -1,33 +1,81 @@
 ﻿using AndroidGlobal = global::Android;
-using Android.App;
-using Android.Content;
-using Android.Media;
-using Android.Provider;
 using MusicPlayer.Models;
 using MusicPlayer.Services;
 using MusicPlayer.ViewModels;
-using Plugin.CurrentActivity;
-using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using XamarinAndroid = Xamarin.Android;
 using Xamarin.Essentials;
+using System.Linq;
+using System;
+using DynamicData;
 
 namespace MusicPlayer.Android
 {
     public class FetchMusicService : IFetchMusicService
     {
+        private List<string> GetAllMountedStorages()
+        {
+            var procMounts = File.ReadAllText("/proc/mounts");
+            string sdCardEntry = string.Empty;
+            if (!string.IsNullOrWhiteSpace(procMounts))
+            {
+                var candidateProcMountEntries = procMounts.Split('\n', '\r').ToList();
+                candidateProcMountEntries.RemoveAll(s => s.IndexOf("storage", StringComparison.OrdinalIgnoreCase) < 0); //leave only stuff mounted in storage
+                candidateProcMountEntries.RemoveAll(s => s.IndexOf("user_id=0,group_id=0", StringComparison.OrdinalIgnoreCase) < 0); // remove stuff we don't have access to
+                candidateProcMountEntries.RemoveAll(s => s.IndexOf("emulated", StringComparison.OrdinalIgnoreCase) > 0); //Remove all emulated stuff
+                if (candidateProcMountEntries.Any())
+                {
+                    for (int i = 0; i < candidateProcMountEntries.Count(); i++)
+                    {
+                        var sdCardEntries = candidateProcMountEntries[i].Split(' ');
+                        candidateProcMountEntries[i] = sdCardEntries.FirstOrDefault(s => s.IndexOf("/storage/", StringComparison.OrdinalIgnoreCase) >= 0);
+                    }
+                    candidateProcMountEntries.Add(AndroidGlobal.OS.Environment.ExternalStorageDirectory.ToString());
+                    return candidateProcMountEntries;
+                }
+                else
+                {
+                    return new List<string>();
+                }
+            }
+            else
+            {
+                return new List<string>();
+            }
+        }
+        List<string> RecursiveSafeFolderSearch(string path)
+        {
+            return new List<string>();
+        }
+
         public IEnumerable<MusicItemViewModel> GetMusicItems()
         {
             //MediaStore.Audio.Media.InterfaceConsts.Id;
             var collection = new ObservableCollection<MusicItemViewModel>();
-            collection.Add(new MusicItemViewModel
+            /*collection.Add(new MusicItemViewModel
             {
                 Title = AndroidGlobal.OS.Environment.ExternalStorageDirectory.ToString()
             });
+            collection.Add(new MusicItemViewModel
+            {
+                Title = AndroidGlobal.OS.Environment.StorageDirectory.ToString()
+            });*/
+            List<string> FilesAndDirs = new List<string>();
+            //FilesAndDirs.AddRange(Directory.EnumerateFileSystemEntries(AndroidGlobal.OS.Environment.ExternalStorageDirectory.ToString()));
+            FilesAndDirs.AddRange(GetAllMountedStorages());
+            foreach (var filepath in FilesAndDirs)
+            {
+                var files = Directory.GetFiles(filepath, "*.mp3",SearchOption.AllDirectories);
+                foreach (var file in files)
+                {
+                    collection.Add(new MusicItemViewModel
+                    {
+                        Title = file
+                    });
+                }
+            }
             /*using (ContentResolver resolver = CrossCurrentActivity.Current.AppContext.ContentResolver)
             {
                 global::Android.Net.Uri? uri = MediaStore.Audio.Media.ExternalContentUri;
