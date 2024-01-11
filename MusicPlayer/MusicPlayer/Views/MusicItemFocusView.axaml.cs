e544a063aa823a18d66a9dd60305c9c1bdada746
Diakon5 <72417806+Diakon5@users.noreply.github.com>
2024-01-11 07:52:45 +0100

    New Views

---
+++ MusicPlayer/MusicPlayer/Views/MusicItemFocusView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace MusicPlayer.Views
+{
+    public partial class MusicItemFocusView : UserControl
+    {
+        public MusicItemFocusView()
+        {
+            InitializeComponent();
+        }
+    }
+}
